ab6368a045e6ced00e030ed16ad47d07e654b483
Kaan Esendemir <esendemirk@gmail.com>
2025-12-31 01:33:26 -0500

    Update JS for icons

--- js/forms.js
+++ js/forms.js
@@ -1,7 +1,7 @@
 // Forms Page JavaScript
 
 // Download form buttons
-document.querySelectorAll('.download-form-btn').forEach(btn => {
+document.querySelectorAll('.download-btn').forEach(btn => {
     btn.addEventListener('click', function() {
         const formTitle = this.closest('.form-card').querySelector('.form-title').textContent;
         alert(`Downloading ${formTitle} form...`);
